2046980eb68af9f93414415bc9d644178e867253
Mikhail Rogov <creynir@gmail.com>
2019-03-26 21:47:17 +0200

    Update Volatile

--- src/Multithreading/VolatileSources/VolatileSources.java
+++ src/Multithreading/VolatileSources/VolatileSources.java
@@ -1,10 +1,14 @@
 package Multithreading.VolatileSources;
 
 public class VolatileSources extends Thread {
-    private volatile boolean running = true;
+    private VolatileData data;
+
+    public VolatileSources(VolatileData data) {
+        this.data = data;
+    }
 
     public void run() {
-        while (running) {
+        while (data.isRunning()) {
             System.out.println("Running");
             try {
                 sleep(100);
@@ -12,21 +16,35 @@ public class VolatileSources extends Thread {
                 e.printStackTrace();
             }
         }
-        System.out.println("Thread stopped");
-    }
-
-    public void shutDown() {
-        running = false;
+        System.out.println("Thread " + Thread.currentThread().getId() + " stopped");
     }
 }
 
 class Main {
     public static void main(String[] args) throws InterruptedException {
-        VolatileSources vl = new VolatileSources();
-        vl.start();
+        VolatileData data = new VolatileData();
+        VolatileSources t = new VolatileSources(data);
+        VolatileSources t2 = new VolatileSources(data);
+        VolatileSources t3 = new VolatileSources(data);
+
+        t.start();
+        t2.start();
+        t3.start();
 
         Thread.sleep(3000);
         System.out.println("Going to set the stop flag to true");
-        vl.shutDown();
+        data.shutDown();
+    }
+}
+
+class VolatileData {
+    private volatile boolean running = true;
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void shutDown() {
+        running = false;
     }
 }
